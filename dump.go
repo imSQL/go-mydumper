83997bb79512e8e965c0f61b00fddf6eba0a6fea
Lei Tian <taylor840326@gmail.com>
2018-04-09 17:57:34 +0800

    update dump.go

--- dump.go
+++ dump.go
@@ -305,7 +305,7 @@ func (d *Dumper) Dump() error {
 	// define arg
 	args := make([]string, 0, 31)
 
-	args = append(args, fmt.Sprintf("--host %s --port %d --user %s --password %s", d.Addr, d.Port, d.User, d.Password))
+	args = append(args, fmt.Sprintf("--host 172.18.10.136 --port 3309 --user root --password 111111"))
 	/*
 		args = append(args, fmt.Sprintf("--port %d", d.Port))
 		args = append(args, fmt.Sprintf("--user %s", d.User))
